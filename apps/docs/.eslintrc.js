5d088a8351a33462ef36255d062727225708e7e2
Travis Kuhl <travis.kuhl@gmail.com>
2024-04-17 10:18:01 -0700

    disable type checks

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -6,4 +6,10 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.js'],
+    },
+  ],
 }
