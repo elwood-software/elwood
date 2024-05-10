d173c14ff93f826e242ff15c37da7cc31a97d8b7
Travis Kuhl <travis.kuhl@gmail.com>
2024-05-10 10:24:15 -0700

    tree

--- packages/react/src/components/files/tree.tsx
+++ packages/react/src/components/files/tree.tsx
@@ -9,78 +9,85 @@ import {Icons} from '@elwood/ui';
 import {toArray} from '@elwood/common';
 import {Link, createNodeLink} from '@/components/link';
 import {FileIcon} from './icon';
+import {Fragment, MouseEvent, MouseEventHandler} from 'react';
 
-export interface FilesTreeProps
-  extends Omit<ITreeViewProps, 'data' | 'nodeRenderer'> {
+export interface FilesTreeProps {
   tree: NodeTree[];
   rootNodeId: string | null;
 }
 
 export function FilesTree(props: FilesTreeProps): JSX.Element {
-  const {tree, rootNodeId, ...treeViewProps} = props;
-  const treeIds = tree.map(item => item.id);
-  const expandedIds = treeViewProps.expandedIds?.filter(item =>
-    treeIds.includes(item as string),
-  );
+  const {tree, rootNodeId} = props;
 
-  if (!rootNodeId || toArray(tree).length === 0) {
-    return <span />;
+  if (!rootNodeId) {
+    return <></>;
   }
 
-  const data: INode<{href: string}>[] = [
-    {
-      name: '',
-      id: rootNodeId,
-      isBranch: true,
-      children: tree
-        .filter(item => item.parent === rootNodeId)
-        .map(item => item.id),
-      parent: null,
-      metadata: {
-        href: '',
-      },
-    },
-    ...tree.map(item => {
-      return {
-        name: item.node.name,
-        id: item.id,
-        isBranch: item.node.type !== 'BLOB',
-        parent: item.parent,
-        children: tree.filter(i => i.parent === item.id).map(i => i.id),
-        metadata: {
-          href: createNodeLink(item.node),
-          path: [...item.node.prefix, item.node.name],
-          nodeType: item.node.type,
-        },
-      };
-    }),
-  ];
+  return <TreeNode parent={rootNodeId} tree={tree} level={0} />;
+}
+
+type TreeNodeProps = {
+  parent: string;
+  tree: NodeTree[];
+  level: number;
+};
+
+function TreeNode(props: TreeNodeProps) {
+  const nodes = props.tree.filter(node => node.parent === props.parent);
+
+  if (nodes.length === 0) {
+    return <></>;
+  }
 
   return (
-    <TreeView
-      data={data}
-      multiSelect
-      nodeRenderer={renderNode}
-      propagateSelectUpwards
-      {...treeViewProps}
-      expandedIds={expandedIds}
-    />
+    <div>
+      {nodes.map(node => {
+        return (
+          <Fragment key={`TreeNodes-${node.id}`}>
+            {node.parent !== 'root' && (
+              <TreeNodeItem
+                name={node.node.name ?? ''}
+                nodeType={node.node.type}
+                href={createNodeLink(node.node)}
+                level={props.level}
+                open={false}
+                onOpenClick={e => {}}
+              />
+            )}
+            {node.node.type !== 'BLOB' ? (
+              <div>
+                <TreeNode
+                  parent={node.node.id}
+                  tree={props.tree}
+                  level={props.level + 1}
+                />
+              </div>
+            ) : null}
+          </Fragment>
+        );
+      })}
+    </div>
   );
 }
 
-function renderNode(props: INodeRendererProps): JSX.Element {
-  const {getNodeProps, element, isBranch, isExpanded, level, handleExpand} =
-    props;
-  const DirIcon = isExpanded ? Icons.ChevronDown : Icons.ChevronRight;
-  const {metadata = {href: '', nodeType: 'TREE'}} = element as INode<{
-    href: string;
-    nodeType: NodeType;
-  }>;
+type TreeNodeItemProps = {
+  open: boolean;
+  onOpenClick: MouseEventHandler;
+  href: string;
+  nodeType: NodeType;
+  name: string;
+  level: number;
+};
+
+function TreeNodeItem(props: TreeNodeItemProps): JSX.Element {
+  const {onOpenClick, level, href, nodeType, name, open = false} = props;
+  const DirIcon = open ? Icons.ChevronDown : Icons.ChevronRight;
+  const isBranch = nodeType !== 'BLOB';
 
   return (
     <div className="flex items-center py-1">
       <button
-        onClick={getNodeProps({onClick: handleExpand}).onClick}
+        onClick={onOpenClick}
         style={{paddingLeft: 10 * (level - 1)}}
         type="button">
         <div className="w-[12px] h-[12px] flex items-center justify-center mr-1.5">
@@ -89,13 +96,13 @@ function renderNode(props: INodeRendererProps): JSX.Element {
           ) : null}
         </div>
       </button>
-      <Link className="flex items-center" href={metadata.href}>
+      <Link className="flex items-center" href={href}>
         <FileIcon
           className="mr-2 text-muted-foreground"
-          nodeType={metadata.nodeType}
+          nodeType={nodeType}
           size={14}
         />
-        <span className="text-sm truncate max-w-[150px]">{element.name}</span>
+        <span className="text-sm truncate max-w-[150px]">{name}</span>
       </Link>
     </div>
   );
