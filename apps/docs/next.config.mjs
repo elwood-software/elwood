c4a809a8337e94c8607f6e141c6ce211e6f8e37b
Travis Kuhl <travis.kuhl@gmail.com>
2024-05-15 12:09:45 -0700

    moved migrations to tle extension. updated migration to use tle

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,11 +1,14 @@
-import withMarkdoc from '@markdoc/next.js'
-import withSearch from './src/markdoc/search.mjs'
+import withMarkdoc from '@markdoc/next.js';
+import withSearch from './src/markdoc/search.mjs';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? 'https://docs.elwood.software' : undefined,
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-}
+};
 
 export default withSearch(
-  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
-)
+  withMarkdoc({schemaPath: './src/markdoc'})(nextConfig),
+);
